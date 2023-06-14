05e21c40f7b22ddab82c3a6cc4eb744417e8d672
21700340JuwonBaek <61731386+21700340JuwonBaek@users.noreply.github.com>
2023-06-14 17:46:39 +0900

    first commit

---
+++ src/main/java/Pack01/Tiger.java
@@ -0,0 +1,16 @@
+package Pack01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Tiger {
+	
+	@RequestMapping("/t1")
+	public String func01() {
+		System.out.println("func01 call");
+		return "TigerView";//클래스가 타이거라서?
+	}
+}
