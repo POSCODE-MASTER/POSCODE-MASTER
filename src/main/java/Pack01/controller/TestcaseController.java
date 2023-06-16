03f9f3dddc07111b645c3d94695fa61d50a6d7ab
woohyeonjoe <qlqkthd8802@gmail.com>
2023-06-16 13:53:56 +0900

    테스트 케이스 동적 추가

--- src/main/java/Pack01/controller/TestcaseController.java
+++ src/main/java/Pack01/controller/TestcaseController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class TestcaseController {
 
@@ -29,43 +31,55 @@ public class TestcaseController {
 
 
     @PostMapping("/testcaseCreate/{problemId}")
-    public String testcaseCreateOk(@PathVariable Long problemId, @RequestParam("input") String input, @RequestParam("output") String output) {
+    public String testcaseCreateOk(@PathVariable Long problemId, @RequestParam("input") List<String> inputs, @RequestParam("output") List<String> outputs) {
 
-        // 입력값을 공백을 기준으로 분할하여 배열로 변환
-        String[] inputValues = input.split("\\s+");
-        String[] outputValues = output.split("\\s+");
-
-        // JSON 배열 생성
-        JsonArray inputArray = new JsonArray();
-        JsonArray outputArray = new JsonArray();
-
-        // 입력값들을 JSON 배열에 추가
-        for (String value : inputValues) {
-            inputArray.add(value);
+        // 입력과 출력 리스트의 크기가 같은지 확인
+        if (inputs.size() != outputs.size()) {
+            throw new IllegalArgumentException("입력과 출력의 개수가 일치하지 않습니다.");
         }
 
-        // 출력값들을 JSON 배열에 추가
-        for (String value : outputValues) {
-            outputArray.add(value);
+        // 입력과 출력 리스트의 크기를 반복문을 돌며 순회하며 테스트케이스 생성 및 저장
+        for (int i = 0; i < inputs.size(); i++) {
+            String input = inputs.get(i);
+            String output = outputs.get(i);
+
+            // 입력값을 공백을 기준으로 분할하여 배열로 변환
+            String[] inputValues = input.split("\\s+");
+            String[] outputValues = output.split("\\s+");
+
+            // JSON 배열 생성
+            JsonArray inputArray = new JsonArray();
+            JsonArray outputArray = new JsonArray();
+
+            // 입력값들을 JSON 배열에 추가
+            for (String value : inputValues) {
+                inputArray.add(value);
+            }
+
+            // 출력값들을 JSON 배열에 추가
+            for (String value : outputValues) {
+                outputArray.add(value);
+            }
+
+            // JSON 객체 생성
+            JsonObject inputJsonObject = new JsonObject();
+            JsonObject outputJsonObject = new JsonObject();
+
+            // 입력 배열과 출력 배열을 JSON 객체에 추가
+            inputJsonObject.add("input", inputArray);
+            outputJsonObject.add("output", outputArray);
+
+            // JSON 문자열로 변환
+            String inputJson = new Gson().toJson(inputJsonObject);
+            String outputJson = new Gson().toJson(outputJsonObject);
+
+            // JSON 문자열을 필요한 로직으로 처리하거나 저장
+            System.out.println(inputJson);
+            System.out.println(outputJson);
+
+            Testcase testcase = new Testcase(problemId, inputJson, outputJson);
+            testCaseService.save(testcase);
         }
-
-        // JSON 객체 생성 및 배열 추가
-        JsonObject inputJsonObject = new JsonObject();
-        JsonObject outputJsonObject = new JsonObject();
-
-        inputJsonObject.add("input", inputArray);
-        outputJsonObject.add("output", outputArray);
-
-        // JSON 문자열로 변환
-        String inputJson = new Gson().toJson(inputJsonObject);
-        String outputJson = new Gson().toJson(outputJsonObject);
-
-        // JSON 문자열을 필요한 로직으로 처리하거나 저장
-        System.out.println(inputJson);
-        System.out.println(outputJson);
-
-        Testcase testcase = new Testcase(problemId, inputJson, outputJson);
-        testCaseService.save(testcase);
 
         return "problemList";
     }
--- src/main/webapp/WEB-INF/Views/testcaseCreate.jsp
+++ src/main/webapp/WEB-INF/Views/testcaseCreate.jsp
@@ -61,6 +61,26 @@
       font-weight: bold;
       margin-bottom: 20px;
     }
+    .form-input-row {
+      display: flex;
+      align-items: center;
+    }
+    .form-input-row .form-input-container {
+      flex: 1;
+      margin-right: 10px;
+    }
+    .add-testcase-btn {
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      padding: 8px 16px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 14px;
+      margin-bottom: 10px;
+      cursor: pointer;
+    }
   </style>
 </head>
 <body>
@@ -71,12 +91,19 @@
     <div class="form-title">Testcase 생성</div>
     <form action="/testcaseCreate/${problemId}" method="POST">
       <div class="form-group">
-        <label for="input">Input</label>
-        <textarea class="form-input-area" id="input" name="input" required></textarea>
+        <div class="form-input-row">
+          <div class="form-input-container">
+            <label for="input">Input</label>
+            <textarea class="form-input-area" id="input" name="input" required></textarea>
+          </div>
+          <div class="form-input-container">
+            <label for="output">Output</label>
+            <textarea class="form-input-area" id="output" name="output" required></textarea>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="output">Output</label>
-        <textarea class="form-input-area" id="output" name="output" required></textarea>
+      <div id="testcase-container" class="form-group">
+        <button class="add-testcase-btn" type="button" onclick="addTestcase()">Testcase 추가</button>
       </div>
       <div class="form-group">
         <button type="submit">Testcase 생성</button>
@@ -84,5 +111,45 @@
     </form>
   </div>
 </div>
+<script>
+  function addTestcase() {
+    var container = document.getElementById("testcase-container");
+    var row = document.createElement("div");
+    row.classList.add("form-input-row");
+
+    var inputContainer = document.createElement("div");
+    inputContainer.classList.add("form-input-container");
+
+    var inputLabel = document.createElement("label");
+    inputLabel.textContent = "Input";
+
+    var inputTextarea = document.createElement("textarea");
+    inputTextarea.classList.add("form-input-area");
+    inputTextarea.setAttribute("name", "input");
+    inputTextarea.setAttribute("required", "true");
+
+    inputContainer.appendChild(inputLabel);
+    inputContainer.appendChild(inputTextarea);
+
+    var outputContainer = document.createElement("div");
+    outputContainer.classList.add("form-input-container");
+
+    var outputLabel = document.createElement("label");
+    outputLabel.textContent = "Output";
+
+    var outputTextarea = document.createElement("textarea");
+    outputTextarea.classList.add("form-input-area");
+    outputTextarea.setAttribute("name", "output");
+    outputTextarea.setAttribute("required", "true");
+
+    outputContainer.appendChild(outputLabel);
+    outputContainer.appendChild(outputTextarea);
+
+    row.appendChild(inputContainer);
+    row.appendChild(outputContainer);
+
+    container.insertBefore(row, container.lastElementChild);
+  }
+</script>
 </body>
 </html>
